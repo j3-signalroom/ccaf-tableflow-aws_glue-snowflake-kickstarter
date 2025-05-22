f707ef8f5d726f99b6be70c8a51e31dc2f1ee821
Jeffrey Jonathan Jennings (J3) <j3@signalroom.io>
2025-05-22 16:18:17 -0400

    See #92.  Refactored.

---
+++ snowflake_user_privileges_tf_module/data.tf
@@ -0,0 +1,45 @@
+data "aws_secretsmanager_secret" "admin_public_keys" {
+  name = "/snowflake_admin_credentials"
+}
+
+data "aws_secretsmanager_secret_version" "admin_public_keys" {
+  secret_id = data.aws_secretsmanager_secret.admin_public_keys.id
+}
+
+data "aws_secretsmanager_secret" "admin_private_key_1" {
+  name = "/snowflake_admin_credentials/rsa_private_key_pem_1"
+}
+
+data "aws_secretsmanager_secret_version" "admin_private_key_1" {
+  secret_id = data.aws_secretsmanager_secret.admin_private_key_1.id
+}
+
+data "aws_secretsmanager_secret" "admin_private_key_2" {
+  name = "/snowflake_admin_credentials/rsa_private_key_pem_2"
+}
+
+data "aws_secretsmanager_secret_version" "admin_private_key_2" {
+  secret_id = data.aws_secretsmanager_secret.admin_private_key_2.id
+}
+
+data "aws_secretsmanager_secret" "svc_public_keys" {
+  name = var.snowflake_secrets_path_prefix
+
+  depends_on = [ 
+    module.snowflake_user_rsa_key_pairs_rotation 
+  ]
+}
+
+data "aws_secretsmanager_secret_version" "svc_public_keys" {
+  secret_id = data.aws_secretsmanager_secret.svc_public_keys.id
+}
+
+locals {
+  rsa_public_key_1  = jsondecode(data.aws_secretsmanager_secret_version.svc_public_keys.secret_string)["rsa_public_key_1"]
+  rsa_public_key_2  = jsondecode(data.aws_secretsmanager_secret_version.svc_public_keys.secret_string)["rsa_public_key_2"]
+  organization_name = "${split("-", jsondecode(data.aws_secretsmanager_secret_version.admin_public_keys.secret_string)["account"])[0]}"
+  account_name      = "${split("-", jsondecode(data.aws_secretsmanager_secret_version.admin_public_keys.secret_string)["account"])[1]}"
+  admin_user        = jsondecode(data.aws_secretsmanager_secret_version.admin_public_keys.secret_string)["admin_user"]
+  private_key       = jsondecode(data.aws_secretsmanager_secret_version.admin_public_keys.secret_string)["active_rsa_public_key_number"] == 1 ? data.aws_secretsmanager_secret_version.admin_private_key_1.secret_string : data.aws_secretsmanager_secret_version.admin_private_key_2.secret_string
+  authenticator     = "SNOWFLAKE_JWT"
+}
