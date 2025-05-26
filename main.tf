9ecdae7310472aa45e0175f21dcf507f91ab39ee
Jeffrey Jonathan Jennings (J3) <j3@signalroom.io>
2025-05-26 09:44:21 -0400

    See #115.  Reverted back to the original enhancement.

--- main.tf
+++ main.tf
@@ -27,25 +27,6 @@ terraform {
     }
 }
 
-module "snowflake_user_rsa_key_pairs_rotation" {   
-    source  = "github.com/j3-signalroom/iac-snowflake-user-rsa_key_pairs_rotation-tf_module"
-
-    # Required Input(s)
-    aws_region           = var.aws_region
-    aws_account_id       = var.aws_account_id
-    snowflake_account    = jsondecode(data.aws_secretsmanager_secret_version.admin_public_keys.secret_string)["account"]
-    service_account_user = local.secrets_insert
-
-    # Optional Input(s)
-    secret_insert             = local.secrets_insert
-    day_count                 = var.day_count
-    aws_lambda_memory_size    = var.aws_lambda_memory_size
-    aws_lambda_timeout        = var.aws_lambda_timeout
-    aws_log_retention_in_days = var.aws_log_retention_in_days
-}
-
-
-# Create the Confluent Cloud Environment
 resource "confluent_environment" "tableflow_kickstarter" {
   display_name = "${local.secrets_insert}"
 
---
+++ snowflake_s3_access_role_tf_module/variables.tf
@@ -0,0 +1,45 @@
+variable "snowflake_aws_role_arn" {
+  description = "The ARN of the Snowflake AWS role."
+  type        = string
+}
+
+variable "s3_bucket_arn" {
+  description = "The ARN S3 bucket."
+  type        = string
+}
+
+variable "aws_s3_integration_name" {
+  description = "The name of the Snowflake Storage Integration for S3."
+  type        = string
+}
+
+variable "base_path" {
+  description = "The base path for the S3 bucket."
+  type        = string
+}
+
+variable "organization_name" {
+  description = "The name of the Snowflake organization."
+  type        = string
+}
+
+variable "account_name" {
+  description = "The name of the Snowflake account."
+  type        = string
+}
+
+variable "admin_user" {
+  description = "The Snowflake admin user."
+  type        = string
+}
+
+variable "authenticator" {
+  description = "The Snowflake authenticator."
+  type        = string
+}
+
+variable "active_private_key" {
+  description = "The active private key for Snowflake authentication."
+  type        = string
+  sensitive   = true
+}
