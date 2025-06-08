cdaf13f72cda9d3f3633dfa49d905a252fba2b8e
Jeffrey Jonathan Jennings (J3) <j3@signalroom.io>
2025-06-08 18:16:27 -0400

    Resolved #205.

--- setup-snowflake-objects.tf
+++ setup-snowflake-objects.tf
@@ -52,6 +52,7 @@ resource "snowflake_stage" "stock_trades" {
   comment             = "Stage for stock trades data from Tableflow Kafka Topic"
 
   depends_on = [
+    confluent_tableflow_topic.stock_trades,
     module.snowflake_glue_s3_access_role,
     snowflake_schema.tableflow_kickstarter,
     snowflake_file_format.parquet
@@ -174,14 +175,13 @@ resource "snowflake_external_table" "stock_trades_with_metadata" {
   }
   
   depends_on = [
-    confluent_tableflow_topic.stock_trades,
     snowflake_stage.stock_trades
   ]
 }
 
-# # Create an external table in Snowflake that references the data in the S3 bucket
-# # that is being populated by the Tableflow Kafka Topic.  This external table will
-# # allow querying the data directly from Snowflake.
+# Create an external table in Snowflake that references the data in the S3 bucket
+# that is being populated by the Tableflow Kafka Topic.  This external table will
+# allow querying the data directly from Snowflake.
 resource "snowflake_external_table" "stock_trades_without_metadata" {
   provider     = snowflake
   database     = local.database_name
@@ -224,7 +224,6 @@ resource "snowflake_external_table" "stock_trades_without_metadata" {
   }
 
   depends_on = [
-    confluent_tableflow_topic.stock_trades,
     snowflake_stage.stock_trades
   ]
 }
