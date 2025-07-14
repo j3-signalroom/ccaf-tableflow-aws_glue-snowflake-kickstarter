3651f7a02ff3f7e1078c8be50377a433be2e2f5d
Jeffrey Jonathan Jennings (J3) <j3@signalroom.io>
2025-07-14 08:04:41 -0400

    See #261.  Refactored.

--- setup-confluent-tableflow.tf
+++ setup-confluent-tableflow.tf
@@ -99,7 +99,7 @@ resource "confluent_tableflow_topic" "stock_trades" {
   ]
 }
 
-resource "confluent_tableflow_topic" "stock_trades_with_totals" {
+resource "confluent_tableflow_topic" "stock_trades" {
   environment {
     id = confluent_environment.tableflow_kickstarter.id
   }
@@ -119,6 +119,7 @@ resource "confluent_tableflow_topic" "stock_trades_with_totals" {
   }
 
   depends_on = [
+    confluent_tableflow_topic.source,
     module.create_set_1
   ]
 }
