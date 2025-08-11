c30a995eacb1a67f150df1595febb37fb3843f7f
Jeffrey Jonathan Jennings (J3) <j3@signalroom.io>
2025-08-11 14:13:53 +0200

    See #310.  Consolidate the grants into the setup-snowflake-user file.

--- setup-snowflake-user.tf
+++ setup-snowflake-user.tf
@@ -72,3 +72,79 @@ resource "snowflake_grant_account_role" "user_system_admin" {
     snowflake_account_role.system_admin_role 
   ]
 }
+
+# Emits GRANT ALL PRIVILEGES ON USER <user_name> TO ROLE <security_admin_role>;
+resource "snowflake_grant_privileges_to_account_role" "user_all_privileges" {
+  provider          = snowflake.security_admin
+  privileges        = ["ALL PRIVILEGES"]
+  account_role_name = snowflake_account_role.security_admin_role.name  
+  on_account_object {
+    object_type = "USER"
+    object_name = local.user_name
+  }
+
+  depends_on = [ 
+    snowflake_grant_account_role.user_security_admin 
+  ]
+}
+
+# Emits GRANT USAGE ON WAREHOUSE <warehouse_name> TO ROLE <security_admin_role>;
+resource "snowflake_grant_privileges_to_account_role" "warehouse_usage" {
+  provider          = snowflake.security_admin
+  privileges        = ["USAGE"]
+  account_role_name = snowflake_account_role.security_admin_role.name
+  on_account_object {
+    object_type = "WAREHOUSE"
+    object_name = local.warehouse_name
+  }
+
+  depends_on = [ 
+    snowflake_grant_account_role.user_security_admin,
+    snowflake_warehouse.tableflow_kickstarter
+  ]
+}
+
+# Emits GRANT USAGE ON DATABASE <database_name> TO ROLE <security_admin_role>;
+resource "snowflake_grant_privileges_to_account_role" "database_usage" {
+  provider          = snowflake.security_admin
+  privileges        = ["USAGE"]
+  account_role_name = snowflake_account_role.security_admin_role.name
+  on_account_object {
+    object_type = "DATABASE"
+    object_name = local.database_name
+  }
+
+  depends_on = [ 
+    snowflake_grant_account_role.user_security_admin,
+    snowflake_database.tableflow_kickstarter
+  ]
+}
+
+# Emits GRANT CREATE STAGE, CREATE FILE FORMAT, CREATE EXTERNAL TABLE, USAGE ON SCHEMA <schema_fully_qualified_name> TO ROLE <security_admin_role>;
+resource "snowflake_grant_privileges_to_account_role" "schema_multiple_grants" {
+  provider          = snowflake.security_admin
+  privileges        = ["CREATE STAGE", "CREATE FILE FORMAT", "CREATE EXTERNAL TABLE", "USAGE"]
+  account_role_name = snowflake_account_role.security_admin_role.name
+  on_schema {
+    schema_name = "${local.database_name}.${local.schema_name}"
+  }
+
+  depends_on = [
+    snowflake_grant_account_role.user_security_admin
+  ]
+}
+
+# Emits GRANT USAGE ON EXTERNAL VOLUME <volume_name> TO ROLE <security_admin_role>;
+resource "snowflake_grant_privileges_to_account_role" "external_volume_usage" {
+  provider          = snowflake.security_admin
+  privileges        = ["USAGE"]
+  account_role_name = snowflake_account_role.security_admin_role.name
+  on_account_object {
+      object_type = "EXTERNAL VOLUME"
+      object_name = local.volume_name
+  }
+
+  depends_on = [
+    snowflake_external_volume.volume
+  ]
+}
