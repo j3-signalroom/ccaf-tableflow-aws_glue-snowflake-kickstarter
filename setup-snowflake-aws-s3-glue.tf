78d55f2eea1711ed39312c233d36b8dfed9eb27d
Jeffrey Jonathan Jennings (J3) <j3@signalroom.io>
2025-08-27 16:03:15 +0200

    See #333.  Refactored.

--- setup-snowflake-aws-s3-glue.tf
+++ setup-snowflake-aws-s3-glue.tf
@@ -60,8 +60,8 @@ resource "aws_iam_policy" "snowflake_s3_glue_role_access_policy" {
           "s3:ListMultipartUploadParts"
         ],
         Resource = [
-          "arn:aws:s3:::${local.tableflow_topic_s3_base_path}", 
-          "arn:aws:s3:::${local.tableflow_topic_s3_base_path}/*"
+          "arn:aws:s3:::${aws_s3_bucket.iceberg_bucket.bucket}", 
+          "arn:aws:s3:::${aws_s3_bucket.iceberg_bucket.bucket}/*"
         ]
       },
       {
@@ -71,7 +71,7 @@ resource "aws_iam_policy" "snowflake_s3_glue_role_access_policy" {
           "s3:ListBucketMultipartUploads",
           "s3:ListBucket"
         ],
-        Resource = "arn:aws:s3:::${local.tableflow_topic_s3_base_path}",
+        Resource = "arn:aws:s3:::${aws_s3_bucket.iceberg_bucket.bucket}",
         Condition = {
           StringLike = {
             "s3:prefix" = ["*"]
